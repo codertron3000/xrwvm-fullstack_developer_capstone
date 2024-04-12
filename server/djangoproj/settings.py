16f33ff256ad558590c7504cad8ac82965219b67
codertron3000 <judah.purcell@gmail.com>
2024-04-12 11:39:46 -0400

    final project complete

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,31 +31,34 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'https://judahpurcell-8000\
         .theianext-0-labs-prod-misc-tools-us-east-0.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
     'https://judahpurcell-8000\
         .theianext-1-labs-prod-misc-tools-us-east-0.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
     'localhost',
     'https://judahpurcell-8000\
         .theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
     'https://judahpurcell-8000\
         .theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
+    'https://judahpurcell-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://judahpurcell-8000\
         .theianext-0-labs-prod-misc-tools-us-east-0.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
     'https://judahpurcell-8000\
         .theianext-1-labs-prod-misc-tools-us-east-0.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
     'https://judahpurcell-8000\
         .theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
     'https://judahpurcell-8000\
         .theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy\
-            .cognitiveclass.ai',
+        .cognitiveclass.ai',
+    'https://judahpurcell-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    
 ]
 
 REST_FRAMEWORK = {
@@ -121,8 +124,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation\
-        .UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
